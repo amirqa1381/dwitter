5a97c4c36a79e3e3bf60de80771fc707ca625b06
Amir Ubunto <amirghasemian1381@gmail.com>
2024-04-20 11:28:18 +0330

    Created the Profile model for profile of the user and for some times that we want to know who follow us or whom we follow

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='User', related_name='profile')
+    image = models.ImageField(upload_to='profile_pics/', verbose_name='Image', blank=True)
+    follows = models.ManyToManyField('self', related_name='followers', symmetrical=False, blank=True)
+
+    def __str__(self):
+        return str(self.user.username)

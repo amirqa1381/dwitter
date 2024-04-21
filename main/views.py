ae34cd36e02abe5d41ea8f46ae09943332226169
Amir Ubunto <amirghasemian1381@gmail.com>
2024-04-21 18:59:15 +0330

    added the profile_list to the views and modified the index_view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from .models import Profile
 
-# Create your views here.
+
+
+def index_page(request):
+    return render(request, "main/index.html")
+
+
+def profile_list(request: HttpRequest):
+    profiles = Profile.objects.exclude(user=request.user)
+    context = {
+        'profiles' : profiles
+    }
+    return render(request, 'main/profile_list.html', context)

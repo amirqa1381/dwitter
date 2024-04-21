5a74dbcd3c6939ef0de0457b938d0c4722ded1e6
Amir Ubunto <amirghasemian1381@gmail.com>
2024-04-21 17:05:24 +0330

    make the user page in the admin dashboard better and customize it

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+
+from .models import Profile
 
 
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user']
+
+
+class InlineProfile(admin.StackedInline):
+    model = Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email']
+    fields = ['username', 'email']
+    inlines = [InlineProfile]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)

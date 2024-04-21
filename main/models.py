5817a937d731417ba539a9f6a9e68a6c65bac1a7
Amir Ubunto <amirghasemian1381@gmail.com>
2024-04-21 17:04:12 +0330

    Added the receiver and sender to the project

--- main/models.py
+++ main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -9,3 +11,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        profile = Profile(user=instance)
+        profile.save()
+        profile.follows.add(instance.profile)
+        profile.save()

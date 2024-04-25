8621e4a3ab6e8d0a6bc5c142e90a4595b21a3e35
Amir Ubunto <amirghasemian1381@gmail.com>
2024-04-25 15:19:53 +0330

    Added a rout and endpoint for the profile_detail that can handel the requests

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index_page, profile_list
+from .views import index_page, profile_list, profile_detail
 
 urlpatterns = [
     path('', index_page, name='index'),
-    path('profile-list/', profile_list, name='profile_list')
+    path('profile-list/', profile_list, name='profile_list'),
+    path('profile-detail/<int:profile_id>/', profile_detail, name='profile_detail'),
 ]

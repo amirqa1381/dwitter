b14256ee2c0999371ddab61adba590f95d57cecb
Amir Ubunto <amirghasemian1381@gmail.com>
2024-04-25 15:19:09 +0330

    Added the profile_detail view for handeling the profile_detail.html and showing the user detail

--- main/views.py
+++ main/views.py
@@ -3,7 +3,6 @@ from django.http import HttpRequest, HttpResponse
 from .models import Profile
 
 
-
 def index_page(request):
     return render(request, "main/index.html")
 
@@ -11,6 +10,18 @@ def index_page(request):
 def profile_list(request: HttpRequest):
     profiles = Profile.objects.exclude(user=request.user)
     context = {
-        'profiles' : profiles
+        'profiles': profiles
     }
     return render(request, 'main/profile_list.html', context)
+
+
+def profile_detail(request: HttpRequest, profile_id):
+    """
+    This function is for showing the profile detail to the user
+    """
+    user_profile = Profile.objects.prefetch_related('follows').get(pk=profile_id)
+    context = {
+        'user_profile': user_profile,
+    }
+    return render(request, 'main/profile_detail.html', context)
+

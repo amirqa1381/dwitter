60f21bb067a480d40ed117d195413d53c0573d3c
Amir Ubunto <amirghasemian1381@gmail.com>
2024-04-26 14:02:01 +0330

    Commited all the things that i need be in the root of the project

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index_page, profile_list, profile_detail
+from .views import profile_list, profile_detail, dashboard
 
 urlpatterns = [
-    path('', index_page, name='index'),
+    # path('', index_page, name='index'),
     path('profile-list/', profile_list, name='profile_list'),
     path('profile-detail/<int:profile_id>/', profile_detail, name='profile_detail'),
+    path('', dashboard, name='dashboard'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,11 +3,17 @@ from django.http import HttpRequest, HttpResponse
 from .models import Profile
 
 
-def index_page(request):
-    return render(request, "main/index.html")
+# def index_page(request):
+#     """
+#     This function is for rendering the main page of the application.
+#     """
+#     return render(request, "main/index.html")
 
 
 def profile_list(request: HttpRequest):
+    """
+    This function is for displaying the all profiles.
+    """
     profiles = Profile.objects.exclude(user=request.user)
     context = {
         'profiles': profiles
@@ -19,6 +25,9 @@ def profile_detail(request: HttpRequest, profile_id):
     """
     This function is for showing the profile detail to the user
     """
+    if not hasattr(request.user, 'profile'):
+        missing_profile = Profile(user=request.user)
+        missing_profile.save()
     user_profile = Profile.objects.prefetch_related('follows').get(pk=profile_id)
     current_user = request.user.profile
     if request.POST:
@@ -33,3 +42,10 @@ def profile_detail(request: HttpRequest, profile_id):
     }
     return render(request, 'main/profile_detail.html', context)
 
+
+def dashboard(request: HttpRequest):
+    """
+    This function is for displaying the dweets of the user's profile that i followed and a form
+    for submitting my dweets
+    """
+    return render(request, 'main/dashboard.html')

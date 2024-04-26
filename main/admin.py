51178d89ade443456663499977efe5f0cd9ea4f8
Amir Ubunto <amirghasemian1381@gmail.com>
2024-04-26 08:48:27 +0330

    Changed something in the admin for correct the saving the user and make the hash password for the user

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.models import User
-
+from django.contrib.auth.admin import UserAdmin
 from .models import Profile
 
 
@@ -14,12 +14,17 @@ class InlineProfile(admin.StackedInline):
     model = Profile
 
 
-class UserAdmin(admin.ModelAdmin):
+class AdminUser(UserAdmin):
     list_display = ['username', 'email']
-    fields = ['username', 'email']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('email',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important Date', {'fields': ('last_login', 'date_joined')}),
+    )
     inlines = [InlineProfile]
 
 
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, AdminUser)
 admin.site.unregister(Group)

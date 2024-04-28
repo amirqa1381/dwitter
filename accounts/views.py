ceb943821cd30ebf7a0f9a8876201dce3cf7633e
Amir Ubunto <amirghasemian1381@gmail.com>
2024-04-28 16:44:53 +0330

    Added the Class base view for handeling the and the name of the class is RegisterView

---
+++ accounts/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from django.views import View
+from .forms import RegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+
+class RegisterView(View):
+    def get(self, request: HttpRequest):
+        form = RegistrationForm()
+        context = {'form': form}
+        return render(request, 'accounts/register_page.html', context)
+
+    def post(self, request: HttpRequest):
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user_name = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            user = User.objects.create_user(user_name, email, password)
+            if first_name or last_name:
+                user.first_name = first_name
+                user.last_name = last_name
+            user.save()
+            messages.success(request, "Account created")
+            return redirect('dashboard')
+        context = {
+            'form': form
+        }
+        return render(request, 'accounts/register_page.html', context)

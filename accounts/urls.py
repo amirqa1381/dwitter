176d778c5f6884d5734744320c5ae3c43e5da861
Amir Ubunto <amirghasemian1381@gmail.com>
2024-05-01 18:56:25 +0330

    Added logout view to the urls and base page for redirection

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import RegisterView, Login
+from .views import RegisterView, logout_view, Login
+from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', Login.as_view(), name='login'),
+    path('logout/', logout_view, name='logout'),
 ]

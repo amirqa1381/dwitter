f7626cee3e7dbf18be5da3f5c3f189a40aee12f6
Amir Ubunto <amirghasemian1381@gmail.com>
2024-05-01 17:45:37 +0330

    added the login url to the urls.py for handeling the urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, Login
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', Login.as_view(), name='login'),
 ]

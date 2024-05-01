c0e819b0dd45e8c1b8e0b7ad489c352dca8f16f6
Amir Ubunto <amirghasemian1381@gmail.com>
2024-05-01 18:55:54 +0330

    Nothing

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from .views import profile_list, profile_detail, dashboard
 
 urlpatterns = [
     # path('', index_page, name='index'),
+    path('', dashboard, name='dashboard'),
     path('profile-list/', profile_list, name='profile_list'),
     path('profile-detail/<int:profile_id>/', profile_detail, name='profile_detail'),
-    path('', dashboard, name='dashboard'),
 ]

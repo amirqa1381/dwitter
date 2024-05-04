fa190f3e802fa31af048bc0d518ca90d718a6b03
Amir Ubunto <amirghasemian1381@gmail.com>
2024-05-04 10:22:02 +0330

    Created a class for Updating the user info

--- accounts/views.py
+++ accounts/views.py
@@ -6,11 +6,14 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import logout
-from .forms import LoginForm
+from .forms import LoginForm, UpdateUserInfoForm
 from django.urls import reverse_lazy
-
+from django.contrib import messages
 
 class RegisterView(View):
+    """
+    This class is for registering the user into the site application.
+    """
     def get(self, request: HttpRequest):
         form = RegistrationForm()
         context = {'form': form}
@@ -38,6 +41,9 @@ class RegisterView(View):
 
 
 class Login(LoginView):
+    """
+    This class is for logging the user into site
+    """
     form_class = LoginForm
     template_name = 'accounts/login_form.html'
     redirect_authenticated_user = True
@@ -47,6 +53,40 @@ class Login(LoginView):
 
 
 def logout_view(request):
+    """
+    This function is for logging out the user from the site
+    """
     logout(request)
     messages.success(request, "You have been logged out.")
     return redirect('dashboard')
+
+
+
+class UpdateUserInfo(View):
+    """
+    This class is for updating the user information.
+    """
+    def get(self, request: HttpRequest):
+        """
+        This function is for get method and when user requested to the site
+        """
+        current_user = User.objects.get(id=request.user.id)
+        form = UpdateUserInfoForm(instance=current_user)
+        context = {'form': form}
+        return render(request, 'accounts/general_edit_info.html', context)
+
+    def post(self, request: HttpRequest):
+        """
+        This function is for the post method and when user sending a things to the site server
+        """
+        if request.user.is_authenticated:
+            current_user = User.objects.get(id=request.user.id)
+            form = UpdateUserInfoForm(request.POST, instance=current_user)
+            if form.is_valid():
+                form.save()
+                messages.success(request,'The Updating the information was successfully :))')
+                return redirect('general_update_info')
+            context = {'form': form}
+            return render(request, 'accounts/general_edit_info.html', context)
+        else:
+            return redirect('login')

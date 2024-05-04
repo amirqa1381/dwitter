7fcf9f8912c582f07691112dce43d6c11cf39523
Amir Ubunto <amirghasemian1381@gmail.com>
2024-05-04 10:21:39 +0330

    Added the UpdateUserInof view to it for handeling the request to the user

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RegisterView, logout_view, Login
-from django.contrib.auth.views import LogoutView
+from .views import RegisterView, logout_view, Login, UpdateUserInfo
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', Login.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
+    path('general-update/', UpdateUserInfo.as_view(), name='general_update_info')
 ]

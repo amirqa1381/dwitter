5488f1e1fcd1bc78155f81cd83d9edbb06ae0674
Amir Ubunto <amirghasemian1381@gmail.com>
2024-05-09 16:55:03 +0330

    Added the UserPasswordChangeView to the ull for handling the endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import RegisterView, logout_view, Login, UpdateUserInfo, SetImageUserOrUpdateUserImage
+from .views import (RegisterView,
+                    logout_view,
+                    Login,
+                    UpdateUserInfo,
+                    SetImageUserOrUpdateUserImage,
+                    UserPasswordChangeView
+                    )
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -7,4 +13,5 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('general-update/', UpdateUserInfo.as_view(), name='general_update_info'),
     path('image/', SetImageUserOrUpdateUserImage.as_view(), name='set_image'),
+    path('password-chenage/', UserPasswordChangeView.as_view(), name='user_password_change')
 ]

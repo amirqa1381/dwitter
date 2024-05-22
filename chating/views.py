8ead3ae5f1301bce3496f370d48f22e98026a716
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-05-22 18:38:26 +0330

    Created two fuction for rooms , one of them is for showing the all the rooms and one of them is for detail of the room

--- chating/views.py
+++ chating/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Room
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest
 
-# Create your views here.
+
+@login_required
+def rooms_view(request: HttpRequest):
+    """
+    this function is for showing rooms in the template
+    """
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'chating/chating_room.html', context)
+
+
+@login_required
+def room_detail_view(request: HttpRequest, room_slug):
+    """
+    This function is for retriving the room and show the detail of it in the templates
+    """
+    room = Room.objects.get(slug=room_slug)
+    context = {'room': room}
+    return render(request, 'chating/chating_room_detail.html', context)

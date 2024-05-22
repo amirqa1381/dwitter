c4d4846a078ca76a826df96c52a71fd5d962134b
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-05-22 19:34:00 +0330

    added the url for handling the views

--- chating/urls.py
+++ chating/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
+from .views import rooms_view, room_detail_view
 
 urlpatterns = [
-
+    path('', rooms_view, name='chating'),
+    path('room/<slug:room_slug>/', room_detail_view, name='room_detail_view'),
 ]

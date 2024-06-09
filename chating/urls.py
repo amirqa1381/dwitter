b82c22ff2510665af2335c65fcf799237bdc6cec
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-06-09 10:54:07 +0330

    some

--- chating/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import rooms_view, room_detail_view
-
-urlpatterns = [
-    path('', rooms_view, name='chating'),
-    path('room/<slug:room_slug>/', room_detail_view, name='room_detail_view'),
-]

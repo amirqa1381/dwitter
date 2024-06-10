ec50b9dfcfa6d045ec3313204c46dac4ac611de3
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-06-10 14:36:29 +0330

    Added the view that i created for deleting to the url for handling it

--- room/urls.py
+++ room/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import rooms, room, CreateRoomChating
+from .views import rooms, room, CreateRoomChating, DeleteRoomView
 
 urlpatterns = [
     path('', rooms, name='rooms'),
     path('create-room/', CreateRoomChating.as_view(), name='create_room-chating'),
     path('<slug:slug>/', room, name='room'),
+    path('<slug:slug>/delete/', DeleteRoomView.as_view(), name='delete_room'),
 ]

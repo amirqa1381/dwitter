7f362f004f9e19b79536ede19c4d397f93ea2b2a
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-06-16 11:37:03 +0330

    added the configuration for the store in the dweeter and added them into the urls and settings

--- Dwitter/urls.py
+++ Dwitter/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('accounts/', include('accounts.urls')),
-    path('rooms/', include('room.urls'))
+    path('rooms/', include('room.urls')),
+    path('store/', include('store.urls')),
 ]
 
 if settings.DEBUG:

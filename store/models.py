febc469b557f01d4c9d892cdb4b6a4ea1ef1ead5
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-06-16 12:37:45 +0330

    Created three class and model for handling store but still i think i should add more models for handling it

--- store/models.py
+++ store/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    this model is used to store categories
+    """
+    name = models.CharField(max_length=150, unique=True, verbose_name='Name')
+    slug = models.SlugField(max_length=250, unique=True, verbose_name='Slug')
+    description = models.TextField(verbose_name='Description')
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """
+    this model is used to store products
+    """
+    name = models.CharField(max_length=150, verbose_name='Name')
+    slug = models.SlugField(max_length=250, unique=True, verbose_name='Slug')
+    description = models.TextField(verbose_name='Description')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Created')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Updated')
+    serial_number = models.PositiveIntegerField(verbose_name='Serial Number')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Category', related_name='product')
+    image = models.ImageField(upload_to='products', verbose_name='Image')
+
+    def __str__(self):
+        return self.name
+
+
+class ProductPrice(models.Model):
+    """
+    this model is used to store product prices
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Product')
+    price = models.PositiveIntegerField(verbose_name='Price')
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Seller')
+
+    def __str__(self):
+        return f"{self.seller.username} / {self.price}"
+

cf2f1df4e658d9ea108a4b1aac2f3e5cbb5ad41b
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-06-18 13:49:07 +0330

    Created new file that is for template that we want to show the form for getting the info and job detail

--- store/views.py
+++ store/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-# this is the view file
-# Create your views here.
+from django.http import HttpRequest
+from django.contrib.auth.models import User
+from django.views import View
+
+
+class UserInformationAndJobDetailsView(View):
+    def get(self, request: HttpRequest):
+        """
+        this function is for routing and handling the get method in the class view
+        """
+        pass
+
+    def post(self, request: HttpRequest):
+        """
+        this function is for routing and handling the post method in the class view
+        """
+        pass

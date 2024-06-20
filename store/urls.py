e5f4099d8fecbb20a23c29faaa07b3c9565c9ecf
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-06-20 14:03:19 +0330

    these file is for handling the inserting the product view and url

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserInformationAndJobDetailsView
+from .views import UserInformationAndJobDetailsView, create_product
 
 
 urlpatterns = [
     path('info-detail/', UserInformationAndJobDetailsView.as_view(), name='user_information'),
+    path('insert-product/', create_product, name='insert_product')
 ]
--- store/views.py
+++ store/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.views import View
-from .forms import UserJobInformationForm
+from .forms import UserJobInformationForm, ProductForm
 from django.contrib import messages
-from .models import UserJobInformation
+from .models import UserJobInformation, Product, ProductPrice
+from django.forms import inlineformset_factory
 
 
 class UserInformationAndJobDetailsView(View):
@@ -33,8 +34,37 @@ class UserInformationAndJobDetailsView(View):
             form.is_submitted = True
             form.save()
             messages.success(request, "Information's of the job and detail of the location was successfully got.")
-            return redirect('dashboard')
+            return redirect('insert_product')
         context = {
             'form': form
         }
         return render(request, 'store/jobandinfo.html', context)
+
+
+def create_product(request: HttpRequest):
+    ProductPriceFormSet = inlineformset_factory(Product, ProductPrice, fields=('price',), extra=1)
+    if request.method == "POST":
+        product_form = ProductForm(request.POST, request.FILES)
+        price_formset = ProductPriceFormSet(request.POST, instance=product_form.instance)
+
+        if product_form.is_valid() and price_formset.is_valid():
+            product = product_form.save()
+            price_formset.instance = product
+            price_formset.seller = request.user
+            price_formset.save()
+            messages.success(request, 'The product that you inserted has created successfully.')
+            return HttpResponseRedirect('insert_product')
+        context = {
+            'product_form': product_form,
+            'price_formset': price_formset
+        }
+        return render(request, 'store/insert-product.html', context)
+    else:
+        product_form = ProductForm()
+        price_formset = ProductPriceFormSet()
+
+    context = {
+        'product_form': product_form,
+        'price_formset': price_formset
+    }
+    return render(request, 'store/insert-product.html', context)

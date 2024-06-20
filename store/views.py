85a379933d1b747e6c85bf5a0713816a4145a8c5
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-06-20 10:11:55 +0330

    i've created a class that name is UserInformationAndJobDetailsView and here we can fill the form and submit it

--- store/views.py
+++ store/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from django.contrib.auth.models import User
 from django.views import View
+from .forms import UserJobInformationForm
+from django.contrib import messages
 
 
 class UserInformationAndJobDetailsView(View):
@@ -9,10 +11,24 @@ class UserInformationAndJobDetailsView(View):
         """
         this function is for routing and handling the get method in the class view
         """
-        pass
+        form = UserJobInformationForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'store/jobandinfo.html', context)
 
     def post(self, request: HttpRequest):
         """
         this function is for routing and handling the post method in the class view
         """
-        pass
+        form = UserJobInformationForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.user = request.user
+            form.save()
+            messages.success(request, "Information's of the job and detail of the location was successfully got.")
+            return redirect('dashboard')
+        context = {
+            'form': form
+        }
+        return render(request, 'store/jobandinfo.html', context)

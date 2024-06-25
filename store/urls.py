990461a80ba2259502cc297899f696d5948d2081
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-06-25 10:21:35 +0330

    Added the ProductListView to the urls for showing the list of the products to the user

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserInformationAndJobDetailsView, CreatProductView
+from .views import UserInformationAndJobDetailsView, CreatProductView, ProductListView
 
 
 urlpatterns = [
+    path('', ProductListView.as_view(), name='store_page'),
     path('info-detail/', UserInformationAndJobDetailsView.as_view(), name='user_information'),
-    path('insert-product/', CreatProductView.as_view(), name='create_product')
+    path('insert-product/', CreatProductView.as_view(), name='create_product'),
 ]
